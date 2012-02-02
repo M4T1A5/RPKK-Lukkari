0c582ab7603656826ec18923f52893b999d01c7f
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2012-02-02 18:41:22 +0200

    Got everything working (hopefully)

--- js/lukkari.js
+++ js/lukkari.js
@@ -4,18 +4,37 @@
 function haeJakso(x){
 	alkupvm = jaksot[x][0].replace(/\./gi, "");
 	loppupvm = jaksot[x][1].replace(/\./gi, "");
-	/* var linkki = "http://www.rpkk.fi/lukkarit/lukkari_show.php?toiminto=luokka&jalku="+alkupvm+"&jloppu="+loppupvm+"&jjakso="+x+"&luokka=D3"; */
-	var linkki = "jakso2.html";
+	var linkki = "http://www.rpkk.fi/lukkarit/lukkari_show.php?toiminto=luokka&jalku="+alkupvm+"&jloppu="+loppupvm+"&jjakso="+x+"&luokka=D3";
+	//var linkki = "jakso2.html";
 	// Yritä ladata lukkari sivulle
-	try{
-		$("#lukkarinimet").load(linkki + " .lukkari_table2", function(){
-			korvaa();
-		});
-		$("#lukkarikoodit").load(linkki + " .lukkari_table2");
+	$("#lukkarinimet").load(linkki + " .lukkari_table2", function(){
+		korvaa();
+	});
+	$("#lukkarikoodit").load(linkki + " .lukkari_table2");
+	$("#jaksoNumero").text("Jakso " + x);
+}
+
+// Hakee nykyisen jakson päivämäärän perusteella
+function nykyinenJakso(){
+	var nyt = Date.today();
+	//var nyt = Date.parse(prompt("Anna testipäivämäärä")); //debug
+
+	for (var i = 1; i <= 6; i++){
+		loppupvm_seuraava = Date.parse(jaksot[i+1][1]);
+		alkupvm = Date.parse(jaksot[i][0]);
+		loppupvm = Date.parse(jaksot[i][1]);
+
+		// Tarkistaa onko nykyinen päivämäärä jonkun jakson sisällä ja palauttaa jakson
+		if(nyt >= alkupvm && nyt <= loppupvm)
+		return i;
+		// Tarkista onko jakso mennyt ohi mutta uusi ei ole vielä alkanut(viikonloput jaksojen välissä)
+		// Ja palautaa uuden jakson
+		else if(nyt < loppupvm_seuraava && nyt >= loppupvm)
+		return i+1;
 	}
-	catch(err){
-		console.log("Lukkarin latauksessa tapahtui virhe " + err);
-	}
+	// Palauttaa viimeisen jakson jos koulu loppunut
+	if(nyt > jaksot[6][1])
+	return 6;
 }
 
 // Korvaa talukosta koodit nimillä
@@ -47,42 +66,16 @@ function ikkunankoko(){
 	}
 }
 
-// Hakee nykyisen jakson päivämäärän perusteella
-function nykyinenJakso(){
-	var nyt = Date.today();
-	//var nyt = Date.parse(prompt("Anna testipäivämäärä")); //debug
-
-	for (var i = 1; i <= 6; i++){
-		loppupvm_seuraava = Date.parse(jaksot[i+1][1]);
-		alkupvm = Date.parse(jaksot[i][0]);
-		loppupvm = Date.parse(jaksot[i][1]);
-
-		// Tarkistaa onko nykyinen päivämäärä jonkun jakson sisällä ja palauttaa jakson
-		if(nyt >= alkupvm && nyt <= loppupvm)
-		return i;
-		// Tarkista onko jakso mennyt ohi mutta uusi ei ole vielä alkanut(viikonloput jaksojen välissä)
-		// Ja palautaa uuden jakson
-		else if(nyt < loppupvm_seuraava && nyt >= loppupvm)
-		return i+1;
-	}
-	// Palauttaa viimeisen jakson jos koulu loppunut
-	if(nyt > jaksot[6][1])
-	return 6;
-}
-
-//$(window).resize(function(){
-//	ikkunankoko();
-//});
-
 $(document).ready(function(){
-
-	//ikkunankoko();
 
 	// Hae oikea jakso ja muuta jaksolistan vakioarvo samaksi
 	haeJakso(nykyinenJakso());
-	$("#jakso").val(nykyinenJakso());
 
 	$("#ruokalista").load("ruokalistaa.php");
+
+	$(".jakso").click(function(){
+		haeJakso($(this).attr("id"));
+	});
 
 	// Vaihtaa nimet koodeiksi ja toisinpäin
 	$("#toggle").click(function(){
